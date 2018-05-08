79080c76de7c2f7ce28a8e91ce242546623c3d7f
DigiBanks99 <wlboshoff@gmail.com>
2018-05-08 10:48:20 +0200

    This is actually to test vscode's git credentials. Added some homescreen data.

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { LivestockService } from './../livestock/livestock.service';
+import { Livestock } from './../livestock/livestock.model';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
+  public animals: Livestock[];
+  private animalsChangedSubscription: Subscription;
+
+  constructor(private livestockService: LivestockService) {
+  }
+
+  ngOnInit() {
+    this.animals = this.livestockService.getLivestock();
+    this.animalsChangedSubscription = this.livestockService.livestockChanged.subscribe((animals: Livestock[]) =>  {
+      this.animals = animals;
+    });
+  }
+
+  ngOnDestroy() {
+    this.animalsChangedSubscription.unsubscribe();
+  }
 }
