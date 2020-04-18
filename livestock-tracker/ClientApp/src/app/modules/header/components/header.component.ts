df33b3c2fb437943cc6ac2ff064e67cf5235eb0e
Wilco Boshoff <wlboshoff@gmail.com>
2020-04-18 21:21:24 +0200

    Reworked layout to use mat-sidenav. Also added a card around the dashboard info.

--- livestock-tracker/ClientApp/src/app/modules/header/components/header.component.ts
+++ livestock-tracker/ClientApp/src/app/modules/header/components/header.component.ts
@@ -1,8 +1,21 @@
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(
+      map((result) => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+}
