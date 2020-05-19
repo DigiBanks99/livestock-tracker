faa82ad62648b0a485066ea500c01b74dd3d84ad
Wilco Boshoff <wlboshoff@gmail.com>
2020-05-19 07:27:19 +0200

    Medical module: medical transaction form

--- livestock-tracker/ClientApp/src/app/modules/feed/components/feeding-transaction-new/feeding-transaction-new.component.ts
+++ livestock-tracker/ClientApp/src/app/modules/feed/components/feeding-transaction-new/feeding-transaction-new.component.ts
@@ -1,6 +1,7 @@
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FeedType } from '@core/models/feed-type.model';
 import { FeedingTransaction } from '@core/models/feeding-transaction.model';
@@ -16,30 +17,46 @@ import { select, Store } from '@ngrx/store';
   selector: 'app-feeding-transaction-new',
   templateUrl: './feeding-transaction-new.component.html',
 })
-export class FeedingTransactionNewComponent implements OnInit {
+export class FeedingTransactionNewComponent implements OnDestroy, OnInit {
   public selectedAnimalId$: Observable<number>;
   public isPending$: Observable<boolean>;
   public error$: Observable<Error>;
   public feedTypes$: Observable<FeedType[]>;
   public unitTypes$: Observable<Unit[]>;
 
+  private destroyed$ = new Subject<void>();
+
   constructor(private store: Store<AppState>, private router: Router) {}
 
-  public ngOnInit() {
+  public ngOnInit(): void {
     this.store.dispatch(new FetchFeedTypes(0, 100, false));
-    this.selectedAnimalId$ = this.store.pipe(select(getSelectedAnimalId));
+    this.selectedAnimalId$ = this.store.pipe(
+      select(getSelectedAnimalId),
+      takeUntil(this.destroyed$)
+    );
     this.isPending$ = this.store.pipe(
       select(
         feedingTransactionStore.selectors.getFeedingTransactionPendingState
-      )
+      ),
+      takeUntil(this.destroyed$)
     );
     this.error$ = this.store.pipe(
-      select(feedingTransactionStore.selectors.getFeedingTransactionErrorState)
+      select(feedingTransactionStore.selectors.getFeedingTransactionErrorState),
+      takeUntil(this.destroyed$)
     );
     this.feedTypes$ = this.store.pipe(
-      select(feedTypeStore.selectors.getFeedTypes)
+      select(feedTypeStore.selectors.getFeedTypes),
+      takeUntil(this.destroyed$)
     );
-    this.unitTypes$ = this.store.pipe(select(getUnits));
+    this.unitTypes$ = this.store.pipe(
+      select(getUnits),
+      takeUntil(this.destroyed$)
+    );
+  }
+
+  public ngOnDestroy(): void {
+    this.destroyed$.next();
+    this.destroyed$.complete();
   }
 
   public onSave(transaction: FeedingTransaction) {
