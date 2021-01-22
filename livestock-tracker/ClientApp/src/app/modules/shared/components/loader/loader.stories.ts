e1b4a2a65bfe4e07370764d57e3e41e18b384184
Wilco Boshoff <wlboshoff@gmail.com>
2021-01-22 20:55:06 +0200

    Fixed loader for animal list

--- livestock-tracker/ClientApp/src/app/modules/shared/components/loader/loader.stories.ts
+++ livestock-tracker/ClientApp/src/app/modules/shared/components/loader/loader.stories.ts
@@ -1,11 +1,9 @@
 import { LoaderModule } from '@shared/components/loader/loader.module';
 import { moduleMetadata } from '@storybook/angular';
-// also exported from '@storybook/angular' if you can deal with breaking changes in 6.1
-import { Meta, Story } from '@storybook/angular/types-6-0';
 
 import { LoaderComponent } from './loader.component';
 
-const meta: Meta = {
+export default {
   title: 'Shared/Loader',
   component: LoaderComponent,
   decorators: [
@@ -15,9 +13,7 @@ const meta: Meta = {
   ]
 };
 
-export default meta;
-
-const Template: Story<LoaderComponent> = (args: LoaderComponent) => ({
+const Template = (args: LoaderComponent) => ({
   component: LoaderComponent,
   props: args
 });
