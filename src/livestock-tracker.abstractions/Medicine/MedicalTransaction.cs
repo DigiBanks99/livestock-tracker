c6369d0ac4a58ad42ba11e26240b852d3ad7086b
Wilco Boshoff <wlboshoff@gmail.com>
2022-01-23 19:41:34 +0200

    Added Medical Transaction integration tests
    Added a new project LivestockTracker.Medicine.IntegrationTests that
    hosts the integratino test relating to medicine and medical
    transactions. It leverages the TestHost feature of ASP.NET Core and
    really simplifies the logic bits for the system.

--- src/livestock-tracker.abstractions/Medicine/MedicalTransaction.cs
+++ src/livestock-tracker.abstractions/Medicine/MedicalTransaction.cs
@@ -1,16 +1,35 @@
-using LivestockTracker.Abstractions.Models.Medical;
 using System;
-using System.ComponentModel.DataAnnotations;
 
 namespace LivestockTracker.Medicine
 {
+    /// <summary>
+    /// A medical transaction for an animal.
+    /// </summary>
     public class MedicalTransaction
     {
+        /// <summary>
+        /// The unique identifier of the transactino.
+        /// </summary>
         public long Id { get; set; }
+        /// <summary>
+        /// The animal the transaction belongs to.
+        /// </summary>
         public long AnimalId { get; set; }
+        /// <summary>
+        /// The unique identifier of the medicine administered.
+        /// </summary>
         public int MedicineId { get; set; }
+        /// <summary>
+        /// The date the medication was administered.
+        /// </summary>
         public DateTimeOffset TransactionDate { get; set; }
+        /// <summary>
+        /// The dose of the medicine administered.
+        /// </summary>
         public decimal Dose { get; set; }
+        /// <summary>
+        /// The identifier of the unit used to measure the administered medicine.
+        /// </summary>
         public int UnitId { get; set; }
     }
 }
