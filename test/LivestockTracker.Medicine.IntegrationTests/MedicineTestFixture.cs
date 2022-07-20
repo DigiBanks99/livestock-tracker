4ed2a190504f0639de77059fedd4ad496cce95f6
Wilco Boshoff <wlboshoff@gmail.com>
2022-07-20 21:29:30 +0200

    Fix deps error on test core library
    Fixed the issue where all tests would pass, but the run would fail
    because the testing CLI thinks the core class lib is a test container.

--- test/LivestockTracker.Medicine.IntegrationTests/MedicineTestFixture.cs
+++ test/LivestockTracker.Medicine.IntegrationTests/MedicineTestFixture.cs
@@ -1,8 +1,3 @@
-using LivestockTracker;
-using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.Data.Sqlite;
-using Microsoft.Extensions.Configuration;
-
 namespace Given;
 
 public class MedicineTestFixture : IntegrationTestFixture
