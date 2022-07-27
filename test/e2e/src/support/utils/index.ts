aecbe58986b7563e2fb356b85a90bb06d30f97e7
Wilco Boshoff <wlboshoff@gmail.com>
2022-07-27 07:35:27 +0200

    Fix e2e tests

---
+++ test/e2e/src/support/utils/index.ts
@@ -0,0 +1,5 @@
+import * as regex from './regex';
+
+export const Regex = {
+  ...regex
+};
---
+++ test/e2e/src/support/utils/regex.ts
@@ -0,0 +1,2 @@
+export const matchDate =
+  /\d+ \b(?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Oct(?:ober)?|(Nov|Dec)(?:ember)?) \d{4}, \d{2}:\d{2}/;
